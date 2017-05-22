ccfcb71b1c154ad646bb5bf4e00980aec79521a4
zhangzhuo <zhangzhuo@b.360.cn>
2017-05-22 18:11:18 +0800

    完善command 命令

--- test_c.py
+++ test_c.py
@@ -4,16 +4,11 @@ from mq_service.work_frame import WORK_FRAME
 import time
 
 
-
-
-
-
-
 def main():
     AMQ_URI = "amqp://user:3^)NB@101.199.126.121:5672/api"
     w = WORK_FRAME("test", auri=AMQ_URI)
-    #w.start()
-    r=w.command("system", "ls -la /")
+    # w.start()
+    r = w.command("system", "ls -la /")
     time.sleep(5)
     print w.get_response(r)
 
