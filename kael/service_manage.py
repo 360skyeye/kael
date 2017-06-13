2b31e3e9d76cb17f51d7531d62a2a47c181daeb4
zhangmengyang <zhangmengyang@b.360.cn>
2017-06-13 21:11:40 +0800

    支持导出crontab类型配置文件，定义crontab类型导出的格式

--- kael/service_manage.py
+++ kael/service_manage.py
@@ -25,27 +25,37 @@ def get_service_group(conf=None):
     {
         'service_group': 'services_default',
         'service_pkg':
-            {
-                'calculate': {
-                    'path': '/data/project/mq-service/services_default/sleep_service',
-                    'version': 1.0,
-                    'services': {
-                        'calculate__add': <function add at 0x00000000029C7AC8>,
-                        'calculate__minus': <function minus at 0x00000000029C7B38>
-                    }
-                },
-                'time': {
-                    'path': '/data/project/mq-service/services_default/time_service',
-                    'version': 1.0,
-                    'services': {
-                        'time__transfer': <function transfer at 0x00000000029C7F28>
-                    }
+        {
+            'calculate': {
+                'path': '/data/project/mq-service/services_default/sleep_service',
+                'version': 1.0,
+                'services': {
+                    'calculate__add': <function add at 0x00000000029C7AC8>,
+                    'calculate__minus': <function minus at 0x00000000029C7B38>
+                }
+            },
+            'time': {
+                'path': '/data/project/mq-service/services_default/time_service',
+                'version': 1.0,
+                'services': {
+                    'time__transfer': <function transfer at 0x00000000029C7F28>
                 }
             }
+        }
+        'crontab_pkg':
+        {
+            'print':{
+                'path':'/data/project/mq-service/services_default/task1_crontab',
+                'version': 1.0,
+                'crontabs':[ '1 * * * *  .....', '1 * * * *  .....' ]
+            }
+
+        }
     }"""
 
-    Services = {'service_pkg': {}}
+    Services = {'service_pkg': {}, 'crontab_pkg': {}}
     service_base_name_exist = set()
+    crontab_name_exist = set()
 
     # 1导入服务包组的配置（配置字典 或 yaml路径）
     if type(conf) is dict:
@@ -68,7 +78,7 @@ def get_service_group(conf=None):
     #  2进入服务包读取服务配置, 根据group配置中的path字段导出service
     for pkg_path in group_setting['path']:
         try:
-            if pkg_path.split(os.path.sep)[0] in ['.', '..']:
+            if pkg_path.split('/')[0] in ['.', '..']:
                 pkg_path = os.path.realpath(os.path.join(service_group_dir, pkg_path))
 
             # 2.1 检查配置中每个服务的Service Base Name是否已存在
@@ -78,11 +88,20 @@ def get_service_group(conf=None):
 
             with open(setting_file) as f:
                 s = yaml.load(f.read())
-                if s['service_base_name'] in service_base_name_exist:
-                    logging.error('Registering from {}'.format(setting_file))
-                    raise Exception('Service Base Name Already Existed: {0}'.format(s['service_base_name']))
+                if s.get('type') == 'service':
+                    if s['service_base_name'] in service_base_name_exist:
+                        logging.error('Registering from {}'.format(setting_file))
+                        raise Exception('Service Base Name Already Existed: {0}'.format(s['service_base_name']))
+                    else:
+                        service_base_name_exist.add(s['service_base_name'])
+                elif s.get('type') == 'crontab':
+                    if s['crontab_name'] in crontab_name_exist:
+                        logging.error('Registering from {}'.format(setting_file))
+                        raise Exception('Crontab Name Already Existed: {0}'.format(s['crontab_name']))
+                    else:
+                        crontab_name_exist.add(s['crontab_name'])
                 else:
-                    service_base_name_exist.add(s['service_base_name'])
+                    raise
 
             # 2.2 找出service_pkg中发布的所有函数，添加到Services字典中
             """
@@ -90,28 +109,43 @@ def get_service_group(conf=None):
             publish_func_names: ['add', 'minus']
             service_name: calculator__add, calculator__minus
             """
-
-            publish_func_names = s['publish'] or []
-            sys.path.append(os.path.dirname(pkg_path))
-            module = __import__('{0}'.format(os.path.basename(pkg_path)), fromlist='dummy')
-            current_pkg_services = {
-                s['service_base_name']: {
-                    'version': s['version'],
-                    'path': pkg_path,
-                    'services': {}
+            if s.get('type') == 'service':
+                publish_func_names = s['publish'] or []
+                sys.path.append(os.path.dirname(pkg_path))
+                module = __import__('{0}'.format(os.path.basename(pkg_path)), fromlist='dummy')
+                current_pkg_services = {
+                    s['service_base_name']: {
+                        'version': s['version'],
+                        'path': pkg_path,
+                        'services': {}
+                    }
                 }
-            }
 
-            try:
-                for func in publish_func_names:
-                    service_name = "{0}__{1}".format(s['service_base_name'], func)
-                    current_pkg_services[s['service_base_name']]['services'].update(
+                try:
+                    for func in publish_func_names:
+                        service_name = "{0}__{1}".format(s['service_base_name'], func)
+                        current_pkg_services[s['service_base_name']]['services'].update(
                             {service_name: getattr(module, func)})
-            except Exception as e:
-                raise ImportError(
+                except Exception as e:
+                    raise ImportError(
                         "{} has not function: {}. Check {}".format(s['service_base_name'], func, setting_file))
 
-            Services['service_pkg'].update(current_pkg_services)
+                Services['service_pkg'].update(current_pkg_services)
+
+            elif s.get('type') == 'crontab':
+                crontab_items = s['publish'] or []
+                # todo 做一些拼接处理
+                # crontab_items = function(crontab_items)
+
+                current_pkg_crontab = {
+                    s['crontab_name']: {
+                        'version': s['version'],
+                        'path': pkg_path,
+                        'crontabs': crontab_items
+                    }
+                }
+                Services['crontab_pkg'].update(current_pkg_crontab)
+
         except Exception as e:
             logging.exception(e)
 
@@ -141,3 +175,11 @@ def update_service_group(conf, service_install_path):
         group_setting.setdefault('path', []).append(service_install_path)
 
     return group_setting
+
+
+if __name__ == '__main__':
+    p = os.path.realpath(os.path.join(cur_dir, '../examples/micro_service/setting.yaml'))
+    s_c = get_service_group(p)
+    from pprint import pprint
+
+    pprint(s_c)
