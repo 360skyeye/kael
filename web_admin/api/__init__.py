a78b2d21c11cb973b56c6613bb58d6343b442529
zhangzhuo <zhangzhuo@b.360.cn>
2017-06-21 12:03:02 +0800

    更改站点结构，抽出通用模块

--- web_admin/api/__init__.py
+++ web_admin/api/__init__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 # Created by zhangzhuo@360.cn on 17/6/20
 from flask import Blueprint
-from .. import app,APP_NAME
-from ..monkey import patch_flask_route,patch_validate_handler
+from .. import app, APP_NAME
+from ..monkey import patch_flask_route, patch_validate_handler
 from flask.config import Config
 
 flag = "/{0}".format(__name__.replace("{0}.".format(APP_NAME), ""))
 blueprint = Blueprint(__name__, __name__, url_prefix=flag)
-
 
 try:
     c = Config("./")
@@ -19,5 +18,5 @@ try:
 except:
     pass
 
-patch_flask_route(blueprint,api=True,json=True)
-patch_validate_handler(APP_NAME,blueprint)
+patch_flask_route(blueprint, api=True, json=True)
+patch_validate_handler(APP_NAME, blueprint)
