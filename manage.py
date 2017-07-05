da7d5f14f2d60ca5e273a16f1f0536e6439031f6
zhangmengyang <zhangmengyang@b.360.cn>
2017-07-05 11:57:10 +0800

    print setting

--- manage.py
+++ manage.py
@@ -5,6 +5,9 @@ from web_admin import app
 
 
 def main():
+    print 'AMQP_URI:', app.config['AMQP_URI']
+    print 'REDIS_URL:', app.config['REDIS_URL']
+
     app.debug = True
     print app.url_map
     server = WSGIServer(("0.0.0.0", 5000), app)
